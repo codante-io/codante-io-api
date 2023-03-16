e2f020ffa3fc6134c1f75384387518d163b377d9
Roberto Cestari <robertotcestari@gmail.com>
2023-03-16 18:16:10 -0300

    add factories and adjust migrations

--- database/migrations/2023_03_16_184919_create_courses_table.php
+++ database/migrations/2023_03_16_184919_create_courses_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
-            $table->string('imageUrl');
+            $table->text('description')->nullable();
+            $table->string('imageUrl')->nullable();
             $table->string('slug');
             $table->timestamps();
         });
