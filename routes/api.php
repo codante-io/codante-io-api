926c1a1cf6f21dd2c504f0f39a30350be732e285
Roberto Cestari <robertotcestari@gmail.com>
2023-03-24 23:30:24 -0300

    api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    if ($request->user()) {
+        return $request->user();
+    }
+    return ["message" => "Não autenticado"];
+});
+
+Route::get('/courses', function () {
+    return \App\Models\Course::all();
+});
+
+Route::get('/courses/{slug}', function ($slug) {
+    return \App\Models\Course::where('slug', $slug)->firstOrFail();
 });
