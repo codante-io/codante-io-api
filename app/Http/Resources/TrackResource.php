545eeeba9ed209b40b4e8d180eac4b7d32e8c798
Roberto Cestari <robertotcestari@gmail.com>
2023-04-12 18:49:43 -0300

    feat: landing page endpoint

--- app/Http/Resources/TrackResource.php
+++ app/Http/Resources/TrackResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class WorkshopResource extends JsonResource
+class TrackResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,7 +14,6 @@ class WorkshopResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-
         $resource = [
             'id' => $this->id,
             'name' => $this->name,
@@ -25,18 +24,10 @@ class WorkshopResource extends JsonResource
             'difficulty' => $this->difficulty,
             'duration_in_minutes' => $this->duration_in_minutes,
             'status' => $this->status,
-            'lessons' => LessonResource::collection($this->whenLoaded('lessons')),
-            'instructor' => new InstructorResource($this->whenLoaded('instructor')),
-            'tags' => TagResource::collection($this->whenLoaded('tags')),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
 
-        if ($this->lessons_count) {
-            $resource['lessons_count'] = $this->lessons_count;
-        }
-
         return $resource;
-        // return parent::toArray($request);
     }
 }
