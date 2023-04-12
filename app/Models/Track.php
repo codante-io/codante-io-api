2bb0597a786846a7b81cee6b4bd9d2ca8cededa2
Roberto Cestari <robertotcestari@gmail.com>
2023-04-12 11:44:04 -0300

    fix many to many polymorphic relationship with tracks

--- app/Models/Track.php
+++ app/Models/Track.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Track extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function workshops()
+    {
+        return $this->morphedByMany(Workshop::class, 'trackable');
+    }
+
+    public function challenges()
+    {
+        return $this->morphedByMany(Challenge::class, 'trackable');
+    }
 }
