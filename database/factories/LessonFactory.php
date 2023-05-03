d81b8c00d3916086bc5158c6742f7bb6efcb4861
Roberto Cestari <robertotcestari@gmail.com>
2023-05-03 11:43:18 -0300

    add video to lesson

--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -21,6 +21,7 @@ class LessonFactory extends Factory
             'description' => fake()->paragraphs(2, true),
             'content' => fake()->paragraphs(10, true),
             'duration_in_seconds' => fake()->numberBetween(60, 3600),
+            'video_url' => 'https://player.vimeo.com/video/112836958',
             'slug' => fake()->slug(4),
         ];
     }
