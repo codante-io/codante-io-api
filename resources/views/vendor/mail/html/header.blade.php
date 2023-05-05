022c48c0441db246a5b55230b0d6679644c09b17
Roberto Cestari <robertotcestari@gmail.com>
2023-05-05 06:27:29 -0300

    logo for darkmode

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,7 @@
     {{-- <a href="{{ $url }}" style="display: inline-block;"> --}}
       <div style="display: inline-block;font-weight: bold; font-size:25px">
         {{-- @if (trim($slot) === 'Laravel') --}}
-        <img src="https://codante.s3.sa-east-1.amazonaws.com/img/codante-logo.png" class="logo" alt="Codante Logo">
+        <img src="https://codante.s3.sa-east-1.amazonaws.com/img/logo/mail-logo.png" class="logo" alt="Codante Logo">
         {{-- @else --}}
         {{-- {{ $slot }} --}}
         {{-- @endif --}}
