59de84e62e96ab90e7cfd2003747cec30ca45f63
Roberto Cestari <robertotcestari@gmail.com>
2023-05-08 07:37:06 -0300

    change wording email

--- resources/views/emails/user-registered.blade.php
+++ resources/views/emails/user-registered.blade.php
@@ -1,14 +1,21 @@
 <x-mail::message>
   # Seja muito bem vindo ao Codante!
 
-  Olá {{ $user->name }}, seja bem vindo ao codante!
+  Olá {{ $user->name }}, agora você faz parte do Codante.io!
 
-  Fique à vontade para explorar nossos workshops e mini-projetos. Qualquer dúvida, estamos à disposição!
+  Fique à vontade para explorar nossos [Workshops](https://codante.io/workshops) e [Mini
+  Projetos](https://codante.io/mini-projetos). Qualquer dúvida, estamos à disposição!
 
-  <x-mail::button :url="'https://codante.io'">
-    Explorar a plataforma
+  Agora, uma pergunta: Você já faz parte da nossa comunidade no Discord? Se não, corre lá e vem bater um papo com a
+  gente!
+
+
+  <x-mail::button :url="'https://discord.gg/PqgmPNS4vg'">
+    Entrar no Discord
   </x-mail::button>
 
+  E claro, qualquer dúvida, chama a gente, ok?
+
   Até mais,<br>
-  {{ config('app.name') }}
+  Equipe Codante
 </x-mail::message>
