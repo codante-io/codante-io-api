d149fadbd2a2541d8b441bc5172b5fd101d90fb2
Roberto Cestari <robertotcestari@gmail.com>
2023-05-08 07:09:23 -0300

    change wording email

--- resources/views/emails/user-joined-challenge.blade.php
+++ resources/views/emails/user-joined-challenge.blade.php
@@ -1,12 +1,12 @@
 <x-mail::message>
-  # Você está participando de um novo desafio!
+  # Você está participando de um novo Mini Projeto!
 
-  Maravilha, {{ $user->name }}! Você está participando do desafio **{{ $challenge->name }}**.
+  Maravilha, {{ $user->name }}! Você está participando do Mini Projeto: **{{ $challenge->name }}**.
 
-  Agora é mão na massa! Acesse o desafio e comece a codar! Qualquer dúvida, estamos à disposição!
+  Agora é mão na massa! Acesse o Mini Projeto e comece a codar! Qualquer dúvida, estamos à disposição!
 
   <x-mail::button :url="'https://codante.io/mini-projetos/' . $challenge->slug">
-    Ver Desafio
+    Ver Mini Projeto
   </x-mail::button>
 
   A gente se vê,<br>
