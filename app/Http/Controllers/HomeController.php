4c618425d3d193b9669c4f792edbdce03ea3d46d
Roberto Cestari <robertotcestari@gmail.com>
2023-05-10 00:32:43 -0300

    add cache home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,49 +10,51 @@ use App\Models\Challenge;
 use App\Models\Track;
 use App\Models\Workshop;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function index()
     {
-
-        return [
-            'featured_workshops' => WorkshopResource::collection(
-                Workshop::query()
-                    ->where('featured', 'landing')
-                    ->where(function ($query) {
-                        $query->where('status', 'published')
-                            ->orWhere('status', 'soon');
-                    })
-                    ->with('lessons')
-                    ->with('instructor')
-                    ->with('tags')
-                    ->get()
-            ),
-            'featured_challenges' => ChallengeResource::collection(
-                Challenge::query()
-                    ->where('featured', 'landing')
-                    ->where(function ($query) {
-                        $query->where('status', 'published')
-                            ->orWhere('status', 'soon');
-                    })
-                    ->with('workshop')
-                    ->with('workshop.lessons')
-                    ->with('tags')
-                    ->get()
-            ),
-            'featured_tracks' => TrackResource::collection(
-                Track::query()
-                    ->where('featured', 'landing')
-                    ->where(function ($query) {
-                        $query->where('status', 'published')
-                            ->orWhere('status', 'soon');
-                    })
-                    ->with('workshops')
-                    ->with('challenges')
-                    ->with('tags')
-                    ->get()
-            )
-        ];
+        return Cache::remember('home_content', 60 * 10, function () {
+            return [
+                'featured_workshops' => WorkshopResource::collection(
+                    Workshop::query()
+                        ->where('featured', 'landing')
+                        ->where(function ($query) {
+                            $query->where('status', 'published')
+                                ->orWhere('status', 'soon');
+                        })
+                        ->with('lessons')
+                        ->with('instructor')
+                        ->with('tags')
+                        ->get()
+                ),
+                'featured_challenges' => ChallengeResource::collection(
+                    Challenge::query()
+                        ->where('featured', 'landing')
+                        ->where(function ($query) {
+                            $query->where('status', 'published')
+                                ->orWhere('status', 'soon');
+                        })
+                        ->with('workshop')
+                        ->with('workshop.lessons')
+                        ->with('tags')
+                        ->get()
+                ),
+                'featured_tracks' => TrackResource::collection(
+                    Track::query()
+                        ->where('featured', 'landing')
+                        ->where(function ($query) {
+                            $query->where('status', 'published')
+                                ->orWhere('status', 'soon');
+                        })
+                        ->with('workshops')
+                        ->with('challenges')
+                        ->with('tags')
+                        ->get()
+                )
+            ];
+        });
     }
 }
