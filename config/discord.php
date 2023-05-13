81127f58eda83c1d46a8d041e8561eb3c48d7bf6
Ederson Rodrigues <ederson@trilhante.com.br>
2023-05-13 11:51:41 -0300

    <feature>: Create discord notification class

---
+++ config/discord.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'channels' => [
+        'notificacoes' => 'https://discord.com/api/webhooks/1106953489839566959/te4Btbt_J3iE-J-TpJp7zBc4jo_1cBhvekCwZ4JN-yhIsXPrZBtsTnoi4VTI7bk8mvOp'
+    ],
+];
