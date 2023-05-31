818a3bb2e24190a881b10b657be59983762c8251
Ederson Rodrigues <ederson@trilhante.com.br>
2023-05-31 14:56:19 -0300

    change webhooks urls

--- config/discord.php
+++ config/discord.php
@@ -2,7 +2,7 @@
 
 return [
     'channels' => [
-        'notificacoes' => 'https://discord.com/api/webhooks/1106953489839566959/te4Btbt_J3iE-J-TpJp7zBc4jo_1cBhvekCwZ4JN-yhIsXPrZBtsTnoi4VTI7bk8mvOp',
-        'bugs' => 'https://discord.com/api/webhooks/1113459877419962438/zJM_NnS5tZc0eFR71TOWaSSuGf85xSwvQ1Qtre9VD082BizdLv2s6j3D8caCbnfxnq3r'
+        'notificacoes' => env('DISCORD_WEBHOOK_NOTIFICACOES'),
+        'bugs' => env('DISCORD_WEBHOOK_BUGS')
     ],
 ];
