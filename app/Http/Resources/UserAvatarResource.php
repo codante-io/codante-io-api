c71aaf8581ffa6baa3903bc4a8be08c8e2af2e65
Roberto Cestari <robertotcestari@gmail.com>
2023-06-02 12:11:46 -0300

    adjust details

--- app/Http/Resources/UserAvatarResource.php
+++ app/Http/Resources/UserAvatarResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource
+class UserAvatarResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
