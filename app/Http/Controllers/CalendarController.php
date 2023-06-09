d428b966524ee2cc0fd76daec82f55d474b3cb96
Roberto Cestari <robertotcestari@gmail.com>
2023-06-08 22:10:13 -0300

    add itens to agenda that still dont have any date

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -10,47 +10,76 @@ use Illuminate\Support\Facades\Schema;
 
 class CalendarController extends Controller
 {
-
     public function showCalendar()
     {
+        // remover colunas desnecessárias
+        $workshopColumns = Schema::getColumnListing("workshops");
+        $workshopColumns = array_diff($workshopColumns, [
+            "description",
+            "video_url",
+            "created_at",
+            "updated_at",
+            "deleted_at",
+        ]);
 
-        $workshopColumns = Schema::getColumnListing('workshops');
-        $workshopColumns = array_diff($workshopColumns, ['description', 'video_url', 'created_at', 'updated_at', 'deleted_at']);
-
-        $challengeColumns = Schema::getColumnListing('challenges');
-        $challengeColumns = array_diff($challengeColumns, ['description', 'video_url', 'created_at', 'updated_at', 'deleted_at']);
-
+        $challengeColumns = Schema::getColumnListing("challenges");
+        $challengeColumns = array_diff($challengeColumns, [
+            "description",
+            "video_url",
+            "created_at",
+            "updated_at",
+            "deleted_at",
+        ]);
 
         // pegar os próximos workshops (soon + data)
         $workshops = Workshop::query()
-            ->where('status', 'soon')
-            ->where('is_standalone', true)
-            ->with('instructor', fn ($query) => $query->select('id', 'name', 'company', 'avatar_url'))
-            ->with('tags', fn ($query) => $query->select('name'))
-            ->whereDate('published_at', '>=', now())
+            ->where("status", "soon")
+            ->where("is_standalone", true)
+            ->with(
+                "instructor",
+                fn ($query) => $query->select(
+                    "id",
+                    "name",
+                    "company",
+                    "avatar_url"
+                )
+            )
+            ->with("tags", fn ($query) => $query->select("name"))
+            ->where(
+                fn ($query) => $query
+                    ->whereDate("published_at", ">=", now())
+                    ->orWhere("published_at", null)
+            )
             ->select($workshopColumns)
             ->get();
 
-
         $challenges = Challenge::query()
-            ->where('status', '!=', 'draft')
-            ->with('tags', fn ($query) => $query->select('name'))
-            ->whereDate('published_at', '>=', now())
+            ->where("status", "!=", "draft")
+            ->with("tags", fn ($query) => $query->select("name"))
+            ->where(
+                fn ($query) => $query
+                    ->whereDate("published_at", ">=", now())
+                    ->orWhere("published_at", null)
+            )
+            ->whereDate("published_at", ">=", now())
             ->select($challengeColumns)
             ->get();
 
+        $challenges->append("type");
+        $workshops->append("type");
 
-
-        $challenges->append('type');
-        $workshops->append('type');
-
-        $merged = $workshops->merge($challenges)->sortBy('published_at');
+        $merged = $workshops->merge($challenges)->sortBy("published_at");
 
         $result = [];
 
         foreach ($merged as $item) {
 
-            $date = $item->published_at->format('Y-m-d');
+            if ($item->published_at == null) {
+                $result['soon'][] = $item;
+                continue;
+            }
+
+            $date = $item->published_at->format("Y-m-d");
             $result[$date][] = $item;
         }
 
