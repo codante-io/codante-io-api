3a329d5a0e1e7b7276a89e0396569a331a45be51
Ederson Rodrigues <ederson@trilhante.com.br>
2023-08-03 12:39:46 -0300

    remove softdeletes from backup_disks

--- app/Models/BackupDisk.php
+++ app/Models/BackupDisk.php
@@ -5,12 +5,10 @@ namespace App\Models;
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BackupDisk extends Model
 {
     use CrudTrait;
-    use SoftDeletes;
 
     protected $table = 'backup_disks';
     protected $fillable = ['id', 'name', 'size_gb', 'local'];
