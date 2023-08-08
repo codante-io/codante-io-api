32d04d01c595702666f0a704137582222e61ef57
Roberto Cestari <robertotcestari@gmail.com>
2023-08-08 16:30:57 -0300

    add laravel backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command("backup:clean")->dailyAt("01:00");
+        // $schedule->command('backup:run --only-to-disk=s3-backup')->dailyAt('02:00');
+        $schedule
+            ->command("backup:run --only-to-disk=s3-backups-db --only-db")
+            ->dailyAt("02:00");
     }
 
     /**
@@ -20,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . "/Commands");
 
-        require base_path('routes/console.php');
+        require base_path("routes/console.php");
     }
 }
