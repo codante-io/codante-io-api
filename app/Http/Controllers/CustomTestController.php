c11bb8ffcfdb8cf2f25f748b2d083b1779aa2b70
Roberto Cestari <robertotcestari@gmail.com>
2023-09-14 16:38:57 -0300

    rollback customtestcontroller

--- app/Http/Controllers/CustomTestController.php
+++ app/Http/Controllers/CustomTestController.php
@@ -3,12 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class CustomTestController extends Controller
 {
     public function handle()
     {
-        return view('custom-test');
     }
 }
