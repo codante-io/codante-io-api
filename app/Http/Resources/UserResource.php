08c5a47df9f110d41f9d8a155db8423bacf7f8d2
Roberto Cestari <robertotcestari@gmail.com>
2023-10-20 07:07:36 -0300

    add user resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "email" => $this->email,
+            "github_id" => $this->github_id,
+            "github_user" => $this->github_user,
+            "avatar_url" => $this->avatar_url,
+            "is_pro" => $this->is_pro,
+            "is_admin" => $this->is_admin,
+            "created_at" => $this->created_at,
+        ];
+    }
+}
