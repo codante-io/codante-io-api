f8a40f51784ed908f94214cc9db892e9523db2e6
Roberto Cestari <robertotcestari@gmail.com>
2023-10-27 17:23:29 -0300

    add stream url to resource

---
+++ database/migrations/2023_10_27_171415_add_streaming_url_field_to_workshops_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table("workshops", function (Blueprint $table) {
+            $table
+                ->string("streaming_url")
+                ->nullable()
+                ->after("status");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table("workshops", function (Blueprint $table) {
+            $table->dropColumn("streaming_url");
+        });
+    }
+};
