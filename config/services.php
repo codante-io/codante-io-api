f13a79331a06308cb63e7b86fbd15ba4e1b73a24
Roberto Cestari <robertotcestari@gmail.com>
2023-11-07 21:12:57 -0300

    wip

--- config/services.php
+++ config/services.php
@@ -42,5 +42,6 @@ return [
 
     "pagarme" => [
         "api_key" => env("PAGARME_API_KEY"),
+        "encryption_key" => env("PAGARME_ENCRYPTION_KEY"),
     ],
 ];
---
+++ resources/views/emails/payment-refunded.blade.php
@@ -0,0 +1,9 @@
+<x-mail::message>
+  # Seu pagamento foi reembolsado, {{ucwords(explode(' ', $user->name)[0])}}!
+
+  Você deverá receber o valor em sua conta de acordo com as políticas do Provedor de Pagamentos. Caso tenha alguma
+  dúvida, entre em contato com o nosso suporte.
+
+  Um abraço, <br />
+  Equipe Codante
+</x-mail::message>
