0ad274743212ccdf5505ad8e8b43f95540449102
Roberto Cestari <robertotcestari@gmail.com>
2023-11-25 14:24:27 -0300

    authorization

---
+++ database/migrations/2023_11_25_132122_rename_field_is_free_from_lessons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table("lessons", function (Blueprint $table) {
+            $table->dropColumn("is_free");
+            $table
+                ->string("available_to")
+                ->after("thumbnail_url")
+                ->default("all") // all | logged_in | pro
+                ->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table("lessons", function (Blueprint $table) {
+            $table
+                ->boolean("is_free")
+                ->default(false)
+                ->index();
+            $table->dropColumn("available_to");
+        });
+    }
+};
