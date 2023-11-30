606621e1c5a78fceab38fefddce40170b2c6b3be
Roberto Cestari <robertotcestari@gmail.com>
2023-11-30 20:19:34 -0300

    send to emailoctopus when user updates the subscription

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -7,8 +7,10 @@ use App\Events\ChallengeForked;
 use App\Events\ChallengeJoined;
 use App\Events\ReactionCreated;
 use App\Events\ReactionDeleted;
+use App\Events\UserStatusUpdated;
 use App\Listeners\AwardPoints;
 use App\Listeners\Registered as RegisteredListener;
+use App\Listeners\UserStatusUpdated as UserStatusUpdatedListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -31,6 +33,7 @@ class EventServiceProvider extends ServiceProvider
         ChallengeForked::class => [AwardPoints::class],
         ReactionCreated::class => [AwardPoints::class],
         ReactionDeleted::class => [AwardPoints::class],
+        UserStatusUpdated::class => [UserStatusUpdatedListener::class],
     ];
 
     /**
