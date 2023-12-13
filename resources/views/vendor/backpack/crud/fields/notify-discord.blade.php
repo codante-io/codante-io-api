a9699e6f07331647dac9981204e191c9f4e9ab86
Roberto Cestari <robertotcestari@gmail.com>
2023-12-13 11:04:55 -0300

    bugfix

--- resources/views/vendor/backpack/crud/fields/notify-discord.blade.php
+++ resources/views/vendor/backpack/crud/fields/notify-discord.blade.php
@@ -1,3 +1,5 @@
+@isset($entry)
 <a target="_blank" href="{{$data['notification-url'] . $entry->getKey()}}" class="btn btn-sm btn-link"><i
     class="la la-bell"></i>
   {{$data['title']}}</a>
+@endisset
