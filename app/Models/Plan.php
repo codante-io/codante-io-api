4b2dc5f5bc525ad53c3b351ec58ae8b073efaeb2
Roberto Cestari <robertotcestari@gmail.com>
2023-12-22 13:54:30 -0300

    add subscriptions to admin panel

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    use HasFactory;
+    use HasFactory, CrudTrait;
+
+    public function subscriptions()
+    {
+        return $this->hasMany(Subscription::class);
+    }
 }
