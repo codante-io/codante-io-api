23073e4b2c5588ab31f5a40af5ebeac196b94195
felipe <felipeavmuller@gmail.com>
2023-12-27 15:39:52 -0300

    wip

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Certificate;
+use App\Models\Challenge;
+use App\Models\ChallengeUser;
+use App\Models\User;
+use App\Models\Workshop;
+use App\Notifications\Discord;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -20,24 +25,58 @@ class CertificateController extends Controller
             "user_id" => "required|string",
             "source_type" => "required|in:workshop,challenge",
             "source_id" => "required|string",
-            "metadata" => "optional|array",
             "status" => "optional|string",
         ]);
 
         $certificateData = [
             'user_id' => $request->user_id,
             'source_type' => $request->source_type,
-            'metadata' => json_encode($request->metadata),
             'status' => $request->status ?? 'pending',
         ];
 
+        $user = User::find($request->user_id);
         if ($request->source_type === 'challenge') {
             $certificateData['challenge_id'] = $request->source_id;
+
+            $challenge = Challenge::find($request->source_id);
+            $challenge_user = ChallengeUser::where('challenge_id', $challenge->id)->where('user_id', $user->id)->first();
+            $certificateData['metadata'] = [
+                [
+                    "duration" => null,
+                    "source_name" => $challenge->name,
+                    "conclusion_date" => $challenge_user->submitted_at ?? now()->format('Y-m-d H:i:s')
+                    // "tags" => $challenge->tags,
+                ]
+            ];
         } elseif ($request->source_type === 'workshop') {
             $certificateData['workshop_id'] = $request->source_id;
+
+            $workshop = Workshop::find($request->source_id);
+            $certificateData['metadata'] = [
+                [
+                    "duration" => $workshop->duration_in_minutes,
+                    "source_name" => $workshop->name,
+                    "conclusion_date" => now(),
+                ]
+            ];
         }
 
         $certificate = Certificate::create($certificateData);
+
+        if ($request->source_type === 'challenge') {
+            new Discord(
+                "💻 {$challenge->name}\n👤 {$user->name}\n🔗 Submissão: <https://codante.io/mini-projetos/{$challenge->slug}/submissoes/{$user->github_user}>\nPara aprovar, substitua o status para published: <https://api.codante.io/admin/certificate/{$certificate->id}/edit>",
+                "pedidos-certificados",
+            );
+        }
+
+        if ($request->source_type === "workshop") {
+            $workshop = Workshop::find($request->source_id);
+            new Discord(
+                "💻 Workshop: {$workshop->name}\n👤 Certificado de Workshop gerado para {$user->name}",
+                "pedidos-certificados",
+            );
+        }
 
         return $certificate;
     }
--- config/discord.php
+++ config/discord.php
@@ -8,5 +8,6 @@ return [
         "submissoes" => env("DISCORD_WEBHOOK_SUBMISSOES"),
         "comunicados" => env("DISCORD_WEBHOOK_COMUNICADOS"),
         "bugs" => env("DISCORD_WEBHOOK_BUGS"),
+        "pedidos-certificados" => env("DISCORD_WEBHOOK_PEDIDOS_CERTIFICADOS"),
     ],
 ];
