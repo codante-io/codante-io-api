923f1db73f658962f956e7ed3b0f1c4b4a3524cb
felipe <felipeavmuller@gmail.com>
2023-12-28 15:00:22 -0300

    add tags e refatora

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\Certificate;
 use App\Models\Challenge;
 use App\Models\ChallengeUser;
+use App\Models\Tag;
 use App\Models\User;
 use App\Models\Workshop;
 use App\Notifications\Discord;
+use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -35,45 +37,44 @@ class CertificateController extends Controller
         ];
 
         $user = User::find($request->user_id);
-        if ($request->source_type === 'challenge') {
-            $certificateData['challenge_id'] = $request->source_id;
+        $source_id = $request->source_id;
+        $source_type = $request->source_type;
 
-            $challenge = Challenge::find($request->source_id);
-            $challenge_user = ChallengeUser::where('challenge_id', $challenge->id)->where('user_id', $user->id)->first();
-            $certificateData['metadata'] = [
-                [
-                    "duration" => null,
-                    "source_name" => $challenge->name,
-                    "conclusion_date" => $challenge_user->submitted_at ?? now()->format('Y-m-d H:i:s')
-                    // "tags" => $challenge->tags,
-                ]
-            ];
-        } elseif ($request->source_type === 'workshop') {
-            $certificateData['workshop_id'] = $request->source_id;
-
-            $workshop = Workshop::find($request->source_id);
-            $certificateData['metadata'] = [
-                [
-                    "duration" => $workshop->duration_in_minutes,
-                    "source_name" => $workshop->name,
-                    "conclusion_date" => now(),
-                ]
-            ];
+        if ($source_type === 'challenge') {
+            $certificateData['challenge_id'] = $source_id;
+            $source = Challenge::find($source_id);
+            $challenge_user = ChallengeUser::where('challenge_id', $source->id)->where('user_id', $user->id)->first();
+            $conclusion_date = $challenge_user->submitted_at ?? now()->format('Y-m-d H:i:s');
+        } elseif ($source_type === 'workshop') {
+            $certificateData['workshop_id'] = $source_id;
+            $source = Workshop::find($source_id);
+            $conclusion_date = now();
         }
+
+        $tag_ids = $source->tags->pluck('id');
+        $tag_names = Tag::whereIn('id', $tag_ids)->pluck('name');
+
+        $certificateData['metadata'] = [
+            [
+                "duration" => $source_type === 'workshop' ? $source->duration_in_minutes : null,
+                "source_name" => $source->name,
+                "conclusion_date" => $conclusion_date,
+                "tags" => $tag_names,
+            ]
+        ];
 
         $certificate = Certificate::create($certificateData);
 
-        if ($request->source_type === 'challenge') {
+        if ($source_type === 'challenge') {
             new Discord(
-                "💻 {$challenge->name}\n👤 {$user->name}\n🔗 Submissão: <https://codante.io/mini-projetos/{$challenge->slug}/submissoes/{$user->github_user}>\nPara aprovar, substitua o status para published: <https://api.codante.io/admin/certificate/{$certificate->id}/edit>",
+                "💻 {$source->name}\n👤 {$user->name}\n🔗 Submissão: <https://codante.io/mini-projetos/{$source->slug}/submissoes/{$user->github_user}>\nPara aprovar, substitua o status para published: <https://api.codante.io/admin/certificate/{$certificate->id}/edit>",
                 "pedidos-certificados",
             );
         }
 
-        if ($request->source_type === "workshop") {
-            $workshop = Workshop::find($request->source_id);
+        if ($source_type === "workshop") {
             new Discord(
-                "💻 Workshop: {$workshop->name}\n👤 Certificado de Workshop gerado para {$user->name}",
+                "💻 Workshop: {$source->name}\n👤 Certificado de Workshop gerado para {$user->name}",
                 "pedidos-certificados",
             );
         }
