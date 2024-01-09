e21cb9e08f3b507bd6c48941876e22af0fdb47c9
Roberto Cestari <robertotcestari@gmail.com>
2024-01-09 19:21:17 -0300

    wip

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -201,15 +201,15 @@ return [
     // The web middleware (group) used in all base & CRUD routes
     // If you've modified your "web" middleware group (ex: removed sessions), you can use a different
     // route group, that has all the the middleware listed below in the comments.
-    "web_middleware" => "web",
+    // "web_middleware" => "web",
     // Or you can comment the above, and uncomment the complete list below.
-    // 'web_middleware' => [
-    //     \App\Http\Middleware\EncryptCookies::class,
-    //     \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-    //     \Illuminate\Session\Middleware\StartSession::class,
-    //     \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-    //     \App\Http\Middleware\VerifyCsrfToken::class,
-    // ],
+    "web_middleware" => [
+        \App\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\VerifyCsrfToken::class,
+    ],
 
     // Set this to false if you would like to use your own AuthController and PasswordController
     // (you then need to setup your auth routes manually in your routes.php file)
@@ -268,10 +268,10 @@ return [
     // The classes for the middleware to check if the visitor is an admin
     // Can be a single class or an array of classes
     "middleware_class" => [
+        \Backpack\CRUD\app\Http\Middleware\UseBackpackAuthGuardInsteadOfDefaultAuthGuard::class,
         App\Http\Middleware\CheckIfAdmin::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Backpack\CRUD\app\Http\Middleware\AuthenticateSession::class,
-        // \Backpack\CRUD\app\Http\Middleware\UseBackpackAuthGuardInsteadOfDefaultAuthGuard::class,
     ],
 
     // Alias for that middleware
