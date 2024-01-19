3ff10a720a3eb6dc81a72aa785e4f92243449675
Roberto Cestari <robertotcestari@gmail.com>
2024-01-19 17:34:45 -0300

    add some tests

---
+++ database/factories/TestimonialFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Testimonial>
+ */
+class TestimonialFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "name" => $this->faker->name,
+            "body" => $this->faker->text,
+            "social_media_link" => $this->faker->url,
+            "social_media_nickname" => $this->faker->userName(),
+            "avatar_url" => $this->faker->url,
+            "company" => $this->faker->company,
+            "source" => $this->faker->company,
+            "featured" => $this->faker->randomElement([null, "landing"]),
+        ];
+    }
+}
