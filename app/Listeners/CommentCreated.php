dceb3e426c87583df51bb7fa16c652d23c6d2dc7
felipe <felipeavmuller@gmail.com>
2024-02-19 11:47:16 -0300

    remove embed link

--- app/Listeners/CommentCreated.php
+++ app/Listeners/CommentCreated.php
@@ -32,7 +32,7 @@ class CommentCreated implements ShouldQueue
 
         // Send Discord notification
         new Discord(
-            "💬 Um novo comentário foi feito por {$event->user->name} em {$event->comment->commentable_type} {$event->comment->commentable_id} {replying to - $replyingTo}: {$event->comment->comment}\n🔗{$event->comment->commentable_url}",
+            "💬 Um novo comentário foi feito por {$event->user->name} em {$event->comment->commentable_type} {$event->comment->commentable_id} {replying to - $replyingTo}: {$event->comment->comment}\n🔗<{$event->comment->commentable_url}>",
             "notificacoes-comentarios"
         );
 
