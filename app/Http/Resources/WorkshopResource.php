c0ab54e6b0da0fddc48d6e44151596bacb6a1480
Ícaro Harry <me@icaro.io>
2024-06-30 11:58:21 -0300

    Refactor WorkshopController and WorkshopResource

--- app/Http/Resources/WorkshopResource.php
+++ app/Http/Resources/WorkshopResource.php
@@ -33,6 +33,7 @@ class WorkshopResource extends JsonResource
             "duration_in_minutes" => $this->duration_in_minutes,
             "status" => $this->status,
             "is_standalone" => $this->is_standalone,
+            "is_premium" => $this->is_premium,
             "lesson_sections" => $this->whenLoaded(
                 "lessons",
                 $this->getLessonSectionsArray()
