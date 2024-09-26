977253344e706e0bbc3ccfc58b145d49d7812702
Roberto Cestari <robertotcestari@gmail.com>
2024-09-26 07:37:51 -0300

    feat: Update LessonResource to include video type and make video_url nullable

--- app/Http/Resources/LessonResource.php
+++ app/Http/Resources/LessonResource.php
@@ -17,23 +17,24 @@ class LessonResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "id" => $this->id,
-            "name" => $this->name,
-            "description" => $this->description,
-            "content" => $this->content,
-            "available_to" => $this->available_to,
-            "user_can_view" => $this->canViewVideo(),
-            "video_url" => $this->canViewVideo() ? $this->video_url : null,
-            "thumbnail_url" => $this->thumbnail_url,
-            "duration_in_seconds" => $this->duration_in_seconds,
-            "slug" => $this->slug,
-            "position" => $this->position,
-            "section" => $this->section,
-            "created_at" => $this->created_at,
-            "updated_at" => $this->updated_at,
-            "user_completed" => $this->user_completed ? true : false,
-            "comments" => Comment::getComments(
-                "App\\Models\\Lesson",
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'content' => $this->content,
+            'available_to' => $this->available_to,
+            'user_can_view' => $this->canViewVideo(),
+            'video_url' => $this->canViewVideo() ? $this->video_url : null,
+            'type' => $this->video_url ? 'video' : 'text',
+            'thumbnail_url' => $this->thumbnail_url,
+            'duration_in_seconds' => $this->duration_in_seconds,
+            'slug' => $this->slug,
+            'position' => $this->position,
+            'section' => $this->section,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'user_completed' => $this->user_completed ? true : false,
+            'comments' => Comment::getComments(
+                'App\\Models\\Lesson',
                 $this->id
             ),
         ];
@@ -41,13 +42,14 @@ class LessonResource extends JsonResource
 
     private function canViewVideo(): bool
     {
-        if (!Auth::check()) {
-            if ($this->available_to === "all") {
+        if (! Auth::check()) {
+            if ($this->available_to === 'all') {
                 return true;
             }
+
             return false;
         }
 
-        return Auth::user()->can("view", $this->resource);
+        return Auth::user()->can('view', $this->resource);
     }
 }
