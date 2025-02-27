212b25de10a5ba87437b3a85c4fb51b863102255
Ícaro Harry <me@icaro.io>
2025-02-27 18:52:31 -0300

    Refactor calendar events endpoint to support dynamic date filtering

---
+++ app/Http/Resources/Calendar/NewWorkshopEventResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources\Calendar;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NewWorkshopEventResource extends JsonResource
+{
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        
+        $event = [
+            'id' => $this->id . '-' . 'workshop',
+            'title' => $this->title ?? $this->name,
+            'description' => $this->short_description,
+            'slug' => $this->slug,
+            'url' => '/workshops/' . $this->slug,
+            'type' => 'workshop',
+            'datetime' => $this->published_at,
+            'image_url' => $this->image_url,
+        ];
+
+
+        return $event;
+    }
+} 
